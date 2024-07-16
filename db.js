b5cf779b2f1ca2a429f5d9c166a6f9a0fc791054
sakinal niraj <sakinalniraj11@gmail.com>
2024-07-16 13:39:01 +0530

    third version

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // connection url
-const mongoURL = 'mongodb://127.0.0.1:27017/learning';
+// const mongoURL = 'mongodb://127.0.0.1:27017/learning';
+const mongoURL = process.env.DB_URL;
 
 // connection function
 const connectToDataBase = ()=>{
